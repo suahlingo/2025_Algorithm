1bc4d634a8809e286a95459d3749e74d432e786f
suahlingo <sgua0720@gmail.com>
2025-06-26 23:30:29 +0900

    [attempt]:5430_AC_졸려 링고

---
+++ Class3/AC_5430.java
@@ -0,0 +1,25 @@
+package Class3;
+
+import java.io.*;
+import java.util.*;
+
+public class AC_5430 {
+
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for(int i=0; i<t; i++){
+            int n = sc.nextInt();
+            String fx="";
+
+            int[] ls = new int[n];
+
+            //흠 .. R이랑 D case문으로 넣고 두개 합쳐지면 그거만큼 n개 원소 리스트 ls에다가 반환하기!
+
+        }
+
+    }
+
+}
