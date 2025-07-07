9842f5082384c6e9642b127944ac66cd26083659
suahlingo <sgua0720@gmail.com>
2025-07-07 21:32:37 +0900

    [attempt]:11659_prefixSum 누적합 노잼

---
+++ Class3/IntervalSum4_11659.java
@@ -0,0 +1,17 @@
+package Class3;
+
+import java.util.Scanner;
+
+public class IntervalSum4_11659 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int m = sc.nextInt();
+
+        int n = sc.nextInt();
+
+    }
+
+}
