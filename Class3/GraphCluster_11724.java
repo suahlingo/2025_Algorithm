041ab4d5802abc395a3e988f96a625e48afcaef6
suahlingo <sgua0720@gmail.com>
2025-07-11 22:07:49 +0900

    [attempt]:11724_DFS

---
+++ Class3/GraphCluster_11724.java
@@ -0,0 +1,17 @@
+package Class3;
+
+import java.util.Scanner;
+
+public class GraphCluster_11724 {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+
+
+    }
+
+}
