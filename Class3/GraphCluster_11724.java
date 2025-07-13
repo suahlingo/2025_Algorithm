c64c86fc8703b2a12a7ec677e484278010afe230
suahlingo <sgua0720@gmail.com>
2025-07-13 14:45:57 +0900

    [solved]:11724_DFS

--- Class3/GraphCluster_11724.java
+++ Class3/GraphCluster_11724.java
@@ -1,17 +1,61 @@
 package Class3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class GraphCluster_11724 {
 
-    public static void main(String[] args){
+    static List<List<Integer>> graph = new ArrayList<>();
+
+    static boolean[] visited;
+
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
 
         int n = sc.nextInt();
         int m = sc.nextInt();
 
+        graph = new ArrayList<>();
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<>());
+        }
 
+        for(int i=0; i<m; i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
 
+        visited = new boolean[n+1];
+
+        int cnt =0;
+        for(int i=1; i<=n; i++){
+            if(!visited[i]){
+                dfs(i);
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+
+        for(int neighbor : graph.get(node)){
+            if(!visited[neighbor]){
+                dfs(neighbor);
+            }
+        }
     }
 
 }
+
+
+
+
+
